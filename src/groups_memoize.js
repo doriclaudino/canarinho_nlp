b7b65cf32983c3cb6fa00ae36fdcf092bc5a8593
doriclaudino <dori.claudino@gmail.com>
2019-09-02 08:35:59 -0400

    saved groups

---
+++ src/groups_memoize.js
@@ -0,0 +1,325 @@
+var groups = {
+  "17023664243-1564443516": {
+    name: "Capivara.cc",
+    isGroup: true,
+    id: "17023664243-1564443516",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "01E90DEA8679997F11891D3C5CA9DC26",
+    createdAt: 1564443516,
+    users: 3
+  },
+  "18572617562-1462832605": {
+    name: "BOMPERFIL.COM 🇧🇷",
+    isGroup: true,
+    id: "18572617562-1462832605",
+    hasUnread: true,
+    unreadCount: 1,
+    lastMessageId: "3AD1BDC51AC8DA41AC98",
+    createdAt: 1462832605,
+    users: 250
+  },
+  "18574178463-1478296738": {
+    name: "GigantesdaResenha sem 😭😭😭",
+    isGroup: true,
+    id: "18574178463-1478296738",
+    hasUnread: true,
+    unreadCount: 1,
+    lastMessageId: "3A95EFDEBA81FB7D6E3C",
+    createdAt: 1478296738,
+    users: 4
+  },
+  "554796451357-1383926947": {
+    name: "Grupo teste",
+    isGroup: true,
+    id: "554796451357-1383926947",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "B9B6F6030601A26B4FE4E9C1B7F1BC8B",
+    createdAt: 1383926947,
+    users: 1
+  },
+  "13478561607-1542236602": {
+    name: "Jessica - Tinder",
+    isGroup: true,
+    id: "13478561607-1542236602",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "833FDA45212094C11E1927C15100F1DE",
+    createdAt: 1542236602,
+    users: 3
+  },
+  "17814750554-1548425433": {
+    name: "Apartamento",
+    isGroup: true,
+    id: "17814750554-1548425433",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "98FCA647E9294DA229EE19AD6D04A43F",
+    createdAt: 1548425433,
+    users: 4
+  },
+  "554799946179-1488680863": {
+    name: "Fotos Estados Unidos",
+    isGroup: true,
+    id: "554799946179-1488680863",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "C0B1EB0255C83A3CE1CAC569A306F5C7",
+    createdAt: 1488680863,
+    users: 42
+  },
+  "554792513179-1462406808": {
+    name: "🍖🍖Assadão 🍖🍖",
+    isGroup: true,
+    id: "554792513179-1462406808",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "D296C468C3D48AC12A63A75660AB16D1",
+    createdAt: 1462406808,
+    users: 24
+  },
+  "554799946179-1412619619": {
+    name: "Extripe 👊",
+    isGroup: true,
+    id: "554799946179-1412619619",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "3A1E6B9DA54A57B83AC5",
+    users: 0
+  },
+  "17814750554-1533656993": {
+    name: "Gadget Street",
+    isGroup: true,
+    id: "17814750554-1533656993",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "3A029A78644738E721C8",
+    users: 0
+  },
+  "17023664243-1530048950": {
+    name: "PERCA PUGBR  🔫🔪💥",
+    isGroup: true,
+    id: "17023664243-1530048950",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "3EB0A83CE362FB861BAA",
+    users: 0
+  },
+  "554799946179-1455502910": {
+    name: "20/02 - Vai more gente 😑🍺",
+    isGroup: true,
+    id: "554799946179-1455502910",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "2182068667",
+    users: 0
+  },
+  "553186428615-1534401761": {
+    name: "✈🇺🇸BOSTON 🇺🇸 U.S.A 🇺🇸 ✈",
+    isGroup: true,
+    id: "553186428615-1534401761",
+    hasUnread: false,
+    unreadCount: 0,
+    users: 0
+  },
+  "554788705659-1422227613": {
+    name: "FaALLaaAmMeEeMMooO🍺🍻🥂🍷🥃🍸",
+    isGroup: true,
+    id: "554788705659-1422227613",
+    hasUnread: false,
+    unreadCount: 0,
+    users: 0
+  },
+  "554598125926-1463702363": {
+    name: "Família Luchtenberg ❤",
+    isGroup: true,
+    id: "554598125926-1463702363",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "13F10D2210EC4B76EEC0B57BA4BE4145",
+    users: 0
+  },
+  "554799946179-1487041564": {
+    name: "Las Vegas Birthday/trip",
+    isGroup: true,
+    id: "554799946179-1487041564",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "3EB0AC00A86140384BA5",
+    users: 0
+  },
+  "17818539019-1511651074": {
+    name: "Snow",
+    isGroup: true,
+    id: "17818539019-1511651074",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "3306310062",
+    users: 0
+  },
+  "554799946179-1485649844": {
+    name: "Pedal trajetos",
+    isGroup: true,
+    id: "554799946179-1485649844",
+    hasUnread: false,
+    unreadCount: 0,
+    users: 0
+  },
+  "554799946179-1484475251": {
+    name: "Escravidão Heidorn",
+    isGroup: true,
+    id: "554799946179-1484475251",
+    hasUnread: false,
+    unreadCount: 0,
+    users: 0
+  },
+  "554788863369-1470756662": {
+    name: "xXSEGURA_OS_MENINOSXx",
+    isGroup: true,
+    id: "554788863369-1470756662",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "03753657B57F24B5F6048E924D781308",
+    users: 0
+  },
+  "5513997641112-1469049933": {
+    name: "Cambada do SQL ",
+    isGroup: true,
+    id: "5513997641112-1469049933",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "4235509311",
+    users: 0
+  },
+  "554791919740-1465394498": {
+    name: "CS 1.6(181.223.35.2:27015",
+    isGroup: true,
+    id: "554791919740-1465394498",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "4FC4AC6A418C75C71AA2F424EDDCE83E",
+    users: 0
+  },
+  "554791252714-1462486137": {
+    name: "Happy Hour?",
+    isGroup: true,
+    id: "554791252714-1462486137",
+    hasUnread: false,
+    unreadCount: 0,
+    users: 0
+  },
+  "554799946179-1453458263": {
+    name: "ORGANIZAÇÃO - Níver 20/02",
+    isGroup: true,
+    id: "554799946179-1453458263",
+    hasUnread: false,
+    unreadCount: 0,
+    users: 0
+  },
+  "554791907775-1455362074": {
+    name: "NVL - TÉCNICO",
+    isGroup: true,
+    id: "554791907775-1455362074",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "E78725435D3AF7CC4224E0C623A3655E",
+    users: 0
+  },
+  "554799261534-1454099668": {
+    name: "🍻🍻 Bar Da Frau  🍻🍻",
+    isGroup: true,
+    id: "554799261534-1454099668",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "493870296",
+    users: 0
+  },
+  "554792513179-1451954209": {
+    name: "Galera da wox🍻😜🍻😜🍻",
+    isGroup: true,
+    id: "554792513179-1451954209",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "2230406720",
+    users: 0
+  },
+  "554791169194-1450105324": {
+    name: "Térététéu 2016 girooO 🍻🍻🍻",
+    isGroup: true,
+    id: "554791169194-1450105324",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "747956445",
+    users: 0
+  },
+  "554799946179-1450141495": {
+    name: "Reunião casa rute 17/11",
+    isGroup: true,
+    id: "554799946179-1450141495",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "1157777837",
+    users: 0
+  },
+  "554799946179-1441643377": {
+    name: "Volley Profissa 👏 🙌",
+    isGroup: true,
+    id: "554799946179-1441643377",
+    hasUnread: false,
+    unreadCount: 0,
+    users: 0
+  },
+  "554799946179-1434931510": {
+    name: "☀🏄 Praia virada 2015 ",
+    isGroup: true,
+    id: "554799946179-1434931510",
+    hasUnread: false,
+    unreadCount: 0,
+    users: 0
+  },
+  "554796204040-1422191965": {
+    name: "Head Shot Group",
+    isGroup: true,
+    id: "554796204040-1422191965",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "2289690405",
+    users: 0
+  },
+  "554788705659-1421716896": {
+    name: "Quinta do peixe 🐟 🍺🍻",
+    isGroup: true,
+    id: "554788705659-1421716896",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "909550469",
+    users: 0
+  },
+  "554791252714-1410552163": {
+    name: "Oracle",
+    isGroup: true,
+    id: "554791252714-1410552163",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "4124595819",
+    users: 0
+  },
+  "554796451357-1398176619": {
+    name: "Juviais 2.0",
+    isGroup: true,
+    id: "554796451357-1398176619",
+    hasUnread: false,
+    unreadCount: 0,
+    users: 0
+  },
+  "554796706179-1398475007": {
+    name: "CEUTI ",
+    isGroup: true,
+    id: "554796706179-1398475007",
+    hasUnread: false,
+    unreadCount: 0,
+    lastMessageId: "90918659",
+    users: 0
+  }
+};
