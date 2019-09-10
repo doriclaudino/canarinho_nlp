f198b6d5bdd4bf75f62916212ad6891c40bd17ce
doriclaudino <dori.claudino@gmail.com>
2019-09-10 15:25:24 -0400

    remove accents, emoji, double space, enter key

--- src/cleanWhatsAppExportData.js
+++ src/cleanWhatsAppExportData.js
@@ -5,23 +5,32 @@ const TfIdf = natural.TfIdf;
 const tfidf = new TfIdf();
 const program = require("commander");
 const pjson = require("../package.json");
+const latinMap = require("./latinMap");
 
 program
   .version(pjson.version)
   .command("import <dir>")
-  .option("-f, --tfidf", "show tfidf frequency analysis")
   .option("-s, --suffix <sufix>", "suffix to save")
+  .option("--tfidf", "show tfidf frequency analysis")
+  .option("--no-accent", "remove accent")
+  .option("--no-emoji", "remove emoji")
   .action(function(dir, options) {
-    const { tfidf = false, suffix = "clean.json" } = options;
-    const saveFileName = dir + suffix;
-    main(dir, tfidf, saveFileName);
+    main(dir, options);
   });
 program.parse(process.argv);
 
-function main(path, tfidf, saveFileName) {
+function main(path, options) {
+  const {
+    tfidf = false,
+    suffix = "clean.json",
+    removeAccent: accent = false,
+    removeEmoji: emoji = false
+  } = options;
+  const saveFileName = path + suffix;
+
   let doc = readFile(path);
   let lines = breakInLines(doc);
-  let cleanedSentences = cleanData(lines);
+  let cleanedSentences = cleanData(lines, removeAccent, removeEmoji);
   saveToDisk(cleanedSentences, saveFileName);
 
   if (tfidf) {
@@ -41,7 +50,7 @@ function breakInLines(document) {
   return tokenizer.tokenize(document);
 }
 
-function cleanData(sentences = []) {
+function cleanData(sentences = [], removeAccent, removeEmoji) {
   if (!sentences.length) return sentences;
 
   const skipThoseMessages = [
@@ -75,9 +84,29 @@ function cleanData(sentences = []) {
     //remove dups/spamm
     if (filteredTokens.find(e => e === removeNewLine)) continue;
 
+    //remove double space
+    removeNewLine = removeNewLine.replace('  ', ' ');    
+
+    if (removeAccent) removeNewLine = removeAccent(removeNewLine);
+    if (removeEmoji) removeNewLine = removeEmoji(removeNewLine);
+
+    //replace enter
+    removeNewLine = removeNewLine.replace('\n', '⏎');
+
     filteredTokens.push(removeNewLine);
   }
   return filteredTokens;
+}
+
+function removeEmoji(str) {
+  const emoji = /([#0-9]\u20E3)|[\xA9\xAE\u203C\u2047-\u2049\u2122\u2139\u3030\u303D\u3297\u3299][\uFE00-\uFEFF]?|[\u2190-\u21FF][\uFE00-\uFEFF]?|[\u2300-\u23FF][\uFE00-\uFEFF]?|[\u2460-\u24FF][\uFE00-\uFEFF]?|[\u25A0-\u25FF][\uFE00-\uFEFF]?|[\u2600-\u27BF][\uFE00-\uFEFF]?|[\u2900-\u297F][\uFE00-\uFEFF]?|[\u2B00-\u2BF0][\uFE00-\uFEFF]?|(?:\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDEFF])[\uFE00-\uFEFF]?|[\u20E3]|[\u26A0-\u3000]|\uD83E[\udd00-\uddff]|[\u00A0-\u269F]/g;
+  return str.replace(emoji, "");
+}
+
+function removeAccent(str) {
+  return str.replace(/[^A-Za-z0-9]/g, letter => {
+    return latinMap[letter] || letter;
+  });
 }
 
 function saveToDisk(data, path) {
