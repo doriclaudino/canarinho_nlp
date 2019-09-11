369ee5ebf0aa987e95ed8ababb500a03dfedd10b
dori claudino <dori.claudino@gmail.com>
2019-09-11 10:37:23 -0400

    refactoring commander

--- src/cleanWhatsAppExportData.js
+++ src/cleanWhatsAppExportData.js
@@ -10,12 +10,16 @@ const latinMap = require("./latinMap");
 program
   .version(pjson.version)
   .command("import <dir>")
-  .option("-s, --suffix <sufix>", "suffix to save")
-  .option("--tfidf", "show tfidf frequency analysis")
-  .option("--no-accent", "remove accent")
-  .option("--no-emoji", "remove emoji")
-  .action(function(dir, options) {
-    crossPath = dir.replace('\\','/')
+  .option("-s, --suffix <suffix>", "suffix to save", ".clean.json")
+  .option("--tfidf", "show tfidf frequency analysis", false)
+  .option("--accent", "allow accents", false)
+  .option("--emoji", "allow emojis", false)
+  .option("--no-lowercase", "transform to lower or maintain the case-sensitive", true)
+  .option("--doublespaces", "allow doublespaces", false)
+  .option("--duplicates", "allow duplicates texts", false)
+  .option("--empty", "allow empty texts", false)
+  .action(function (dir, options) {
+    crossPath = dir.replace('\\', '/')
     main(crossPath, options);
   });
 program.parse(process.argv);
@@ -23,15 +27,19 @@ program.parse(process.argv);
 function main(path, options) {
   const {
     tfidf = false,
-    suffix = "clean.json",
-    removeAccent: accent = false,
-    removeEmoji: emoji = false
+      suffix,
+      accent,
+      emoji,
+      lowercase,
+      doublespaces,
+      duplicates,
+      empty
   } = options;
   const saveFileName = path + suffix;
 
   let doc = readFile(path);
   let lines = breakInLines(doc);
-  let cleanedSentences = cleanData(lines, removeAccent, removeEmoji);
+  let cleanedSentences = cleanData(lines, accent, emoji, lowercase, doublespaces, duplicates, empty);
   saveToDisk(cleanedSentences, saveFileName);
 
   if (tfidf) {
@@ -51,7 +59,15 @@ function breakInLines(document) {
   return tokenizer.tokenize(document);
 }
 
-function cleanData(sentences = [], removeAccent, removeEmoji) {
+function cleanData(sentences = [], allowAccent, allowEmoji, lowerCase, allowDoubleSpaces, allowDuplicates, allowEmpty) {
+  console.log(`
+  allowAccent: ${allowAccent}, 
+  allowEmoji: ${allowEmoji}, 
+  lowerCase: ${lowerCase}, 
+  allowDoubleSpaces: ${allowDoubleSpaces},
+  allowDuplicates: ${allowDuplicates},
+  allowEmpty: ${allowEmpty}
+  `)
   if (!sentences.length) return sentences;
 
   const skipThoseMessages = [
@@ -74,44 +90,70 @@ function cleanData(sentences = [], removeAccent, removeEmoji) {
     });
     if (foundAndSkip) continue;
 
-    removeHeadDateAndUserPhone = line
-      .toLocaleLowerCase()
-      .replace(/.{1,10}\:\d{1,2}\s.{1,50}\:\s/gim, "");
-    removeNewLine = removeHeadDateAndUserPhone
-      .toLocaleLowerCase()
-      .replace(/\n$/, "");
-    removeNewLine = removeNewLine.toLocaleLowerCase().replace(/^\n/, "");
+    let copyLine = line
+    if (lowerCase == true)
+      copyLine = line.toLocaleLowerCase()
+
+    copyLine = removeLineDataHeader(copyLine);
+    copyLine = removeEndLineBreakLine(copyLine);
+    copyLine = removeInitialBreakLine(copyLine);
 
     //remove dups/spamm
-    if (filteredTokens.find(e => e === removeNewLine)) continue;
+    if (!allowDuplicates && filteredTokens.find(e => e === copyLine)) continue;
 
-    //remove double space
-    removeNewLine = removeNewLine.replace('  ', ' ');    
-
-    if (removeAccent) removeNewLine = removeAccent(removeNewLine);
-    if (removeEmoji) removeNewLine = removeEmoji(removeNewLine);
+    if (!allowAccent) copyLine = removeAccent(copyLine);
+    if (!allowEmoji) copyLine = removeEmoji(copyLine);
 
     //replace enter
-    removeNewLine = removeNewLine.replace(/\n/gmi, '⏎');
+    copyLine = copyLine.replace(/\n/gmi, '⏎');
 
-    filteredTokens.push(removeNewLine);
+    //remove double space
+    if (!allowDoubleSpaces)
+      copyLine = removeDoubleSpaces(copyLine)
+
+    //ignore empty strings
+    if (!allowEmpty && isEmpty(copyLine)) continue;
+
+
+    filteredTokens.push(copyLine);
   }
   return filteredTokens;
 }
 
+function isEmpty(str) {
+  return str.trim().length === 0
+}
+
+function removeLineDataHeader(str) {
+  return str.replace(/.{1,10}\:\d{1,2}\s.{1,50}\:\s/gim, "");
+}
+
+function removeInitialBreakLine(str) {
+  return str.replace(/^\n/gmi, "");
+}
+
+function removeEndLineBreakLine(str) {
+  return str.replace(/\n$/gmi, "");
+}
+
+function removeDoubleSpaces(str) {
+  const regex = /\s{2,}/gmi;
+  return str.replace(regex, '')
+}
+
 function removeEmoji(str) {
-  const emoji = /([#0-9]\u20E3)|[\xA9\xAE\u203C\u2047-\u2049\u2122\u2139\u3030\u303D\u3297\u3299][\uFE00-\uFEFF]?|[\u2190-\u21FF][\uFE00-\uFEFF]?|[\u2300-\u23FF][\uFE00-\uFEFF]?|[\u2460-\u24FF][\uFE00-\uFEFF]?|[\u25A0-\u25FF][\uFE00-\uFEFF]?|[\u2600-\u27BF][\uFE00-\uFEFF]?|[\u2900-\u297F][\uFE00-\uFEFF]?|[\u2B00-\u2BF0][\uFE00-\uFEFF]?|(?:\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDEFF])[\uFE00-\uFEFF]?|[\u20E3]|[\u26A0-\u3000]|\uD83E[\udd00-\uddff]|[\u00A0-\u269F]/g;
+  const emoji = /([#0-9]\u20E3)|[\xA9\xAE\u203C\u2047-\u2049\u2122\u2139\u3030\u303D\u3297\u3299][\uFE00-\uFEFF]?|[\u2190-\u21FF][\uFE00-\uFEFF]?|[\u2300-\u23FF][\uFE00-\uFEFF]?|[\u2460-\u24FF][\uFE00-\uFEFF]?|[\u25A0-\u25FF][\uFE00-\uFEFF]?|[\u2600-\u27BF][\uFE00-\uFEFF]?|[\u2900-\u297F][\uFE00-\uFEFF]?|[\u2B00-\u2BF0][\uFE00-\uFEFF]?|(?:\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDEFF])[\uFE00-\uFEFF]?|[\u20E3]|[\u26A0-\u3000]|\uD83E[\udd00-\uddff]|[\u00A0-\u269F]/gmi;
   return str.replace(emoji, "");
 }
 
 function removeAccent(str) {
-  return str.replace(/[^A-Za-z0-9]/g, letter => {
+  return str.replace(/[^A-Za-z0-9]/gmi, letter => {
     return latinMap[letter] || letter;
   });
 }
 
 function saveToDisk(data, path) {
-  fs.writeFileSync(path, JSON.stringify(data, null, 2));
+  fs.writeFileSync(path, JSON.stringify(data, null, 1));
 }
 
 function wordFrequency(sentences) {
@@ -127,7 +169,7 @@ function wordFrequency(sentences) {
 }
 
 function printWordFrequency() {
-  tfidf.listTerms(0 /*document index*/).forEach(function(item) {
+  tfidf.listTerms(0 /*document index*/ ).forEach(function (item) {
     console.log(item.term + ": " + item.tfidf);
   });
 }
